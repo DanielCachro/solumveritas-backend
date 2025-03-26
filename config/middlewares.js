768d352d7c8c4a20045f2e8e4a7959d75199a0ce
DanielCachro <danielcachro@gmail.com>
2025-03-26 15:42:07 +0100

    feat: configure CORS middleware with dynamic allowed domains

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+module.exports = ({env}) => [
+	'strapi::logger',
+	'strapi::errors',
+	'strapi::security',
+	{
+		name: 'strapi::cors',
+		config: {
+			origin: env.array('ALLOWED_DOMAINS'),
+			keepHeaderOnError: true,
+		},
+	},
+	'strapi::poweredBy',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
+]
